52e0e3a612407e284914bca10005929bba700aaa
Ahmad Ansorudin <65240136+ansorudin@users.noreply.github.com>
2022-10-09 01:44:42 +0700

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+const Layout = ({ children, title }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title || "Pretest PrivyID Frontend"}</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
